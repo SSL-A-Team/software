dba921ba17a5a4f88960e0983cd8f1aa7ef706b5
JNeiger <fcdneiger@gmail.com>
2021-08-17 22:19:58 -0500

    Update autoref to protobuf and multicast receiver move

--- ateam_autoref_bridge/src/autoref_bridge_node.cpp
+++ ateam_autoref_bridge/src/autoref_bridge_node.cpp
@@ -1,8 +1,10 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
+
+#include <ateam_common/multicast_receiver.hpp>
 #include <ssl_league_msgs/msg/referee.hpp>
 
-#include "referee_multicast_bridge.hpp"
+#include "message_conversions.hpp"
 
 namespace ateam_autoref_bridge
 {
@@ -12,14 +14,23 @@ class AutorefBridgeNode : public rclcpp::Node
 public:
   explicit AutorefBridgeNode(const rclcpp::NodeOptions& options)
   : rclcpp::Node("autoref_bridge", options),
-    multicast_bridge_([this](const auto& msg){ referee_publisher_->publish(msg);})
+    multicast_rcevier_("224.5.23.1",
+                       10003,
+                       [this](auto* buffer, size_t bytes_received){
+                         Referee referee_proto;
+                         if (!referee_proto.ParseFromArray(buffer, bytes_received))
+                           return false;
+
+                         referee_publisher_->publish(message_conversions::fromProto(referee_proto));
+                         return true;
+                       })
   {
     referee_publisher_ = create_publisher<ssl_league_msgs::msg::Referee>("~/referee_messages", rclcpp::SystemDefaultsQoS());
   }
 
 private:
   rclcpp::Publisher<ssl_league_msgs::msg::Referee>::SharedPtr referee_publisher_;
-  RefereeMulticastBridge multicast_bridge_;
+  MulticastReceiver multicast_rcevier_;
 };
 
 }
