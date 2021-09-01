7ed416891a0fdaad22e2aea935181400b88c6915
JNeiger <fcdneiger@gmail.com>
2021-09-01 19:08:17 -0400

    style

--- ateam_common/include/ateam_common/multicast_receiver.hpp
+++ ateam_common/include/ateam_common/multicast_receiver.hpp
@@ -1,9 +1,30 @@
-#ifndef MULTICAST_RECEIVER_HPP
-#define MULTICAST_RECEIVER_HPP
+// Copyright 2021 A Team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
-#include <functional>
+#ifndef ATEAM_COMMON__MULTICAST_RECEIVER_HPP_
+#define ATEAM_COMMON__MULTICAST_RECEIVER_HPP_
 
 #include <boost/asio.hpp>
+
+#include <functional>
+#include <string>
 
 namespace ateam_common
 {
@@ -14,11 +35,12 @@ public:
    * @param char* Data received in latest packet
    * @param size_t Length of data received
    */
-  using ReceiveCallback = std::function<void(char*, size_t)>;
+  using ReceiveCallback = std::function<void (char *, size_t)>;
 
-  MulticastReceiver(std::string multicast_ip_address,
-                         short multicast_port,
-                         ReceiveCallback receive_callback);
+  MulticastReceiver(
+    std::string multicast_ip_address,
+    int16_t multicast_port,
+    ReceiveCallback receive_callback);
 
   ~MulticastReceiver();
 
@@ -30,9 +52,9 @@ private:
   std::array<char, 1024> buffer_;
   std::thread io_service_thread_;
 
-  void HandleMulticastReceiveFrom(const boost::system::error_code& error, size_t bytes_received);
+  void HandleMulticastReceiveFrom(const boost::system::error_code & error, size_t bytes_received);
 };
 
-}
+}  // namespace ateam_common
 
-#endif // AUTOREF_BRIDGE_HPP
+#endif  // ATEAM_COMMON__MULTICAST_RECEIVER_HPP_
--- ateam_ssl_vision_bridge/src/message_conversions.hpp
+++ ateam_ssl_vision_bridge/src/message_conversions.hpp
@@ -1,5 +1,25 @@
-#ifndef MESSAGE_CONVERSIONS_HPP
-#define MESSAGE_CONVERSIONS_HPP
+// Copyright 2021 A Team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef MESSAGE_CONVERSIONS_HPP_
+#define MESSAGE_CONVERSIONS_HPP_
 
 #include <ssl_league_msgs/msg/vision_detection_ball.hpp>
 #include <ssl_league_msgs/msg/vision_detection_robot.hpp>
@@ -30,6 +50,6 @@ ssl_league_msgs::msg::VisionGeometryData fromProto(const SSL_GeometryData & prot
 
 ssl_league_msgs::msg::VisionWrapper fromProto(const SSL_WrapperPacket & proto_msg);
 
-}
+}  // namespace ateam_ssl_vision_bridge::message_conversions
 
-#endif // MESSAGE_CONVERSIONS_HPP
+#endif  // MESSAGE_CONVERSIONS_HPP_
