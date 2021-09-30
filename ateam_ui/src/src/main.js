c9e86062c001d916bb597d9d30ff0cf992806e3f
chachmu <mwoodward@gatech.edu>
2021-09-29 23:10:00 -0500

    Vuetify installed but havent fully figured out how to use it

--- ateam_ui/src/src/main.js
+++ ateam_ui/src/src/main.js
@@ -1,4 +1,11 @@
+import 'vuetify/styles'
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import colors from 'vuetify/lib/util/colors'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const vuetify = createVuetify({theme: {defaultTheme: 'dark'}});
+const app = createApp(App).use(vuetify).mount('#app');
+
+console.log(Object.getOwnPropertyNames(vuetify))
