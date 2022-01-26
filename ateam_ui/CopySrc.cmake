77b2c9ecab81f564f86608fca8ca62bd643067ad
chachmu <mwoodward@gatech.edu>
2022-01-25 18:53:10 -0600

    Fix cmake again? Also add --quiet and -host

---
+++ ateam_ui/CopySrc.cmake
@@ -0,0 +1,47 @@
+# Get recursive list of files in ./src
+# Copyright 2021 A Team
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+file(GLOB_RECURSE src_files
+  LIST_RECURSE true
+  RELATIVE ${SOURCE_DIR}/src
+  ${SOURCE_DIR}/src/*
+)
+
+# Add file_list to ./src files
+list(APPEND src_files file_list)
+
+# Copy ./src to install directory
+foreach(src_file ${src_files})
+  if(${src_file} STREQUAL file_list)
+    configure_file(
+      ${BINARY_DIR}/file_list
+      ${INSTALL_PREFIX}/share/${PROJECT_NAME}/src/${src_file}
+      COPYONLY
+    )
+  else()
+    configure_file(
+      ${SOURCE_DIR}/src/${src_file}
+      ${INSTALL_PREFIX}/share/${PROJECT_NAME}/src/${src_file}
+      COPYONLY
+    )
+  endif()
+endforeach()
