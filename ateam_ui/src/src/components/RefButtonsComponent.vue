c8cbe006d317a2078a1750b6c42811e2024af18d
Matthew Barulic <matthew.barulic@gmail.com>
2022-06-14 00:19:21 -0400

    Refactors GC component a bit and support disabling buttons when disconnected.

--- ateam_ui/src/src/components/RefButtonsComponent.vue
+++ ateam_ui/src/src/components/RefButtonsComponent.vue
@@ -5,22 +5,28 @@
             id="gc_start_button"
             color="green"
             class="d-flex my-1 justify-space-around"
-            v-on:click="sendGCCommand('NORMAL_START')">Start</v-btn>
+            v-on:click="sendGCCommand('NORMAL_START', 'UNKNOWN')"
+            :disabled="!normalStartAllowed">Start</v-btn>
         <v-btn
             id="gc_force_start_button"
             color="green-darken-4"
             class="d-flex my-1 justify-space-around"
-            v-on:click="sendGCCommand('FORCE_START')">Force Start</v-btn>
+            v-on:click="sendGCCommand('FORCE_START', 'UNKNOWN')"
+            :disabled="!forceStartAllowed">Force Start</v-btn>
         <v-btn
             id="gc_stop_button"
             color="red"
             class="d-flex my-1 justify-space-around"
-            v-on:click="sendGCCommand('STOP')">Stop</v-btn>
+            v-on:click="sendGCCommand('STOP', 'UNKNOWN')"
+            :disabled="!stopAllowed">Stop</v-btn>
         <v-btn
             id="gc_halt_button"
             color="red-darken-4"
             class="d-flex my-1 justify-space-around"
-            v-on:click="sendGCCommand('HALT')">Halt</v-btn>
+            v-on:click="sendGCCommand('HALT', 'UNKNOWN')"
+            :disabled="!haltAllowed">Halt</v-btn>
+        <!-- TODO(barulicm) Add a settings modal for chaning GC address / reconnecting. -->
+        <!-- TODO(barulicm) Add buttons / drop down for sending team-specific commands (ie. blue kickoff). -->
     </v-container>
 </template>
 
@@ -29,32 +35,65 @@
 export default {
     data: function() {
         return {
-            connection: null
+            gcAddress: "ws://localhost:8081",
+            connection: null,
+            connectionState: WebSocket.CLOSED
         }
     },
     created: function() {
-        console.log("Opening API websocket connection to GC")
-        // TODO(barulicm) The GC IP and port should be parameterized. Not sure how.
-        this.connection = new WebSocket("ws://localhost:8081/api/control")
-        this.connection.onmessage = function(event) {
-            // TODO(barulicm) enable / disable buttons based on game state?
-        }
-        this.connection.onopen = function(event) {
-            console.log("Successfully connected to the GC API server.")
-        }
-        this.connection.onerror = function(event) {
-            console.log(event)
-            document.getElementById("gc_start_button").disabled = true;
-            document.getElementById("gc_force_start_button").disabled = true;
-            document.getElementById("gc_stop_button").disabled = true;
-            document.getElementById("gc_halt_button").disabled = true;
-        }
+        this.attemptToConnect()
+    },
+    beforeUnmount: function() {
+        this.connection.close()
     },
     methods: {
-        sendGCCommand: function(command_type) {
-            console.log("Sending GC command: " + command_type)
-            this.connection.send('{ "change": {"origin":"UI", "revertible":true, "newCommand":{"command":{"type":"' + command_type + '", "forTeam":"UNKNOWN"}}}}')
+        sendGCCommand: function(command_type, team) {
+            this.connection.send(JSON.stringify({
+                change: {
+                    origin: "UI",
+                    revertible: true,
+                    newCommand: {
+                        command: {
+                            type: command_type,
+                            forTeam: team
+                        }
+                    }
+                }
+            }))
+        },
+        attemptToConnect: function() {
+            var vue_object = this
+            this.connection = new WebSocket(this.gcAddress + "/api/control")
+            this.connection.onmessage = function(event) {
+                // TODO(barulicm) enable / disable buttons based on game state?
+            }
+            this.connection.onopen = function(event) {
+                console.log("Connected to the GC API server.")
+                vue_object.connectionState = event.target.readyState
+            }
+            this.connection.onerror = function(event) {
+                console.log("Error communicating with GC API server:")
+                console.log(event)
+                vue_object.connectionState = event.target.readyState
+            }
+            this.connection.onclose = function(event) {
+                vue_object.connectionState = event.target.readyState
+            }
         }
+    },
+    computed: {
+        normalStartAllowed() {
+            return this.connectionState === WebSocket.OPEN
+        },
+        forceStartAllowed() {
+            return this.connectionState === WebSocket.OPEN
+        },
+        stopAllowed() {
+            return this.connectionState === WebSocket.OPEN
+        },
+        haltAllowed() {
+            return this.connectionState === WebSocket.OPEN
+        },
     }
 }
 </script>
