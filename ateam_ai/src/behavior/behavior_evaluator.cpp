a785122aaae501ce7c89386c7e9e0b453728bfb9
JNeiger <fcdneiger@gmail.com>
2023-01-25 21:47:16 -0800

    Fix bad merge

--- ateam_ai/src/behavior/behavior_evaluator.cpp
+++ ateam_ai/src/behavior/behavior_evaluator.cpp
@@ -75,10 +75,9 @@ DirectedGraph<BehaviorGoal> BehaviorEvaluator::get_best_behaviors(const World & 
 
   // Generate 16 move behaviors towards the ball, we'll figure out which
   // ones we can fill later
-<<<<<<< HEAD
   // DirectedGraph<BehaviorGoal> simple_move;
   // static double j = 0;
-  // j += 0.005;
+  // j += kRotationSpeed;
   // for (int i = 0; i < 16; i++) {
   //   double theta = i / 11.0 * 3.14 * 2 + j;
   //   BehaviorGoal move{
@@ -87,21 +86,7 @@ DirectedGraph<BehaviorGoal> BehaviorEvaluator::get_best_behaviors(const World & 
   //     MoveParam(
   //       (sin(3 * theta) + 1.5) * Eigen::Vector2d{cos(theta), sin(
   //           theta)} - Eigen::Vector2d{2, 0})};
-  //   simple_move.add_node(move);
   // }
-=======
-  DirectedGraph<BehaviorGoal> simple_move;
-  static double j = 0;
-  j += kRotationSpeed;
-  for (int i = 0; i < 16; i++) {
-    double theta = i / 11.0 * 3.14 * 2 + j;
-    BehaviorGoal move{
-      BehaviorGoal::Type::MoveToPoint,
-      BehaviorGoal::Priority::Required,
-      MoveParam(
-        (sin(3 * theta) + 1.5) * Eigen::Vector2d{cos(theta), sin(
-            theta)} - Eigen::Vector2d{2, 0})};
->>>>>>> main
 
   //
   // Qual Video Behaviors
@@ -175,11 +160,7 @@ DirectedGraph<BehaviorGoal> BehaviorEvaluator::get_best_behaviors(const World & 
   // choose direct shot because score chance is better or
   // maybe the total behavior completetion time is short
   // or maybe the other one can't be completed due to number of robots
-<<<<<<< HEAD
   DirectedGraph<BehaviorGoal> behavior_out = qual_goalie_and_shot;
-=======
-  DirectedGraph<BehaviorGoal> behavior_out = simple_move;
->>>>>>> main
 
   return behavior_out;
 }
