7574eae4aecfe246b1afb22895ee63fc0b837313
Matthew Barulic <matthew.barulic@gmail.com>
2023-05-07 17:54:17 -0400

    Starts filling out rnp helper tests.

--- ateam_radio_bridge/test/unit_tests/rnp_packet_helpers_tests.cpp
+++ ateam_radio_bridge/test/unit_tests/rnp_packet_helpers_tests.cpp
@@ -2,14 +2,37 @@
 
 #include <rnp_packet_helpers.hpp>
 
+// TODO(barulicm) expand test coverage
+
 TEST(SetCRC, BasicTest)
 {
-
+  RadioPacket packet{
+    0,
+    1,
+    2,
+    CC_ACK,
+    0,
+    {}
+  };
+  ateam_radio_bridge::SetCRC(packet);
+  EXPECT_EQ(packet.crc32, 1560025497u);
 }
 
 TEST(HasCorrectCRC, BasicTest)
 {
+  RadioPacket packet{
+    1560025497,
+    1,
+    2,
+    CC_ACK,
+    0,
+    {}
+  };
+  EXPECT_TRUE(ateam_radio_bridge::HasCorrectCRC(packet));
 
+  
+  packet.crc32 = 12; // arbitrary bad CRC value
+  EXPECT_FALSE(ateam_radio_bridge::HasCorrectCRC(packet));
 }
 
 TEST(SetDataPayload, HelloRequest)
