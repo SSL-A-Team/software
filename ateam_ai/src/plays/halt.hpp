3aab4b0991876bdc495e4defdf6abca0bd5f4142
Christian Clark <christianclark687@gmail.com>
2023-06-25 17:17:36 -0400

    Add back old plays

---
+++ ateam_ai/src/plays/halt.hpp
@@ -0,0 +1,44 @@
+// Copyright 2023 A Team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef PLAYS__HALT_HPP_
+#define PLAYS__HALT_HPP_
+
+#include "types/world.hpp"
+#include "types/behavior_goal.hpp"
+
+#include <Eigen/Dense>
+
+DirectedGraph<BehaviorGoal> generate_halt(const World & world) {
+    DirectedGraph<BehaviorGoal> halt_graph;
+    for (std::size_t id = 0; id < world.our_robots.size(); id++) {
+        // Get the current position of each robot
+        const Eigen::Vector2d robot_position_ = world.our_robots.at(id).value().pos;
+        // Tell it to go to that position
+        BehaviorGoal halt {
+            BehaviorGoal::Type::MoveToPoint,
+            BehaviorGoal::Priority::Required,
+            MoveParam(Eigen::Vector2d{robot_position_.x(), robot_position_.y()})
+        };
+        halt_graph.add_node(halt);
+    }
+    return halt_graph;
+}
+#endif // PLAYS__HALT_HPP
---
+++ ateam_ai/src/plays/shoot.hpp
@@ -0,0 +1,53 @@
+// Copyright 2023 A Team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef PLAYS__SHOOT_HPP_
+#define PLAYS__SHOOT_HPP_
+
+#include <Eigen/Dense>
+
+#include "util/directed_graph.hpp"
+#include "types/behavior_goal.hpp"
+
+
+DirectedGraph<BehaviorGoal> generate_basic_shoot(
+    const World & world, const Field & field
+    ){
+    DirectedGraph<BehaviorGoal> basic_shoot;
+
+    // Get the center of the opponent's goal from the field geometry
+    Eigen::Vector2d _goal_center = Eigen::Vector2d(
+        // Our side is always negative, so kick to the positive side at the edge
+        field.field_length  / 2,
+        // The center of the goal is at the field center (0)
+        0
+    );
+    // Tell one robot to shoot
+    BehaviorGoal shoot {
+        BehaviorGoal::Type::PivotKick,
+        BehaviorGoal::Priority::Required,
+        // Kick to the center of their goal
+        KickParam(_goal_center)
+    };
+    basic_shoot.add_node(shoot);
+
+    return basic_shoot;
+};
+#endif // PLAYS__SHOOT_HPP_
