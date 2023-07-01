41b6b0673aba2642710756e4df745e556894a68f
Christian Clark <christianclark687@gmail.com>
2023-06-30 20:24:27 -0400

    Work done on defense play

--- ateam_ai/src/plays/defense.hpp
+++ ateam_ai/src/plays/defense.hpp
@@ -25,38 +25,46 @@
 #include <Eigen/Dense>
 #include <array>
 #include <vector>
+#include <optional>
 
 #include "types/world.hpp"
 #include "types/behavior_goal.hpp"
 #include "ateam_geometry/utilities.hpp"
+#include "ateam_geometry/ateam_geometry.hpp"
 
 const FieldSidedInfo & our_side_info
 DirectedGraph<BehaviorGoal> generate_basic_defense(
   const World & world,
+  const Feild & field,
   const FieldSidedInfo & our_side_info)
 {
   DirectedGraph<BehaviorGoal> defense_graph;
   // Go to the middle of the goalie area
   BehaviorGoal goalie = get_goalie_behavior_goal(our_side_info);
   defense_graph.add_node(goalie);
+
+  std::vector<BehaviorGoal> defenders = get_defense_behavior_goals(world, field, 2);
+  for (BehaviorGoal defender : defenders){
+    defense_graph.add_node(defender);
+  }
+  return defense_graph;
 }
 
-// TODO(Christian) : Fix this
 BehaviorGoal get_goalie_behavior_goal(const FieldSidedInfo & our_side_info)
 {
-  // Sort the corners with top right at index 0 and bottom left at index 2
-  std::array<Eigen::Vector2d> defense_area = std::sort(
-    our_side_info.goalie_corners.begin(),
-    our_side_info.goalie_corners.end(), ateam_geometry::sort_eigen_2d_high_low());
+  // Line that is 0.5 m from the defense area in all directions
+  ateam_geometry::Segment goalie_line = ateam_geometry::Segment(ateam_geometry::Point(-4, 0.5), ateam_geometry::Point(-4, -0.5));
 
-  Eigen::Vector2d _goalie_point = Eigen::Vector2d(
-    // Here I'm assuming these are opposite corners of the goal
-    // Does this need to be negative to match our conventions?
-    (our_side_info.goalie_corners.at(0).x() + our_side_info.goalie_corners.at(2).x()) / 2,
-    (our_side_info.goalie_corners.at(0).y() + our_side_info.goalie_corners.at(2).y()) / 2
-  );
+  // Get the ball location
+  std::optional<Eigen::Vector2d> ball_location = world.get_unique_ball();
+  while (!ball_location.has_value()) {
+    ball_location = world.get_unique_ball();
+  }
 
-  // Have the goalie defend the goal
+  // Get the point on the goalie line that is closest to the ball
+  ateam_geometry::Point _goalie_point = goalie_line.projection(ball_location);
+
+  // Have the goalie defend the goal by going to that point
   return BehaviorGoal goalie {
     BehaviorGoal::Type::MoveToPoint,
     BehaviorGoal::Priority::Required,
@@ -82,7 +90,7 @@ std::vector<BehaviorGoal> get_defense_behavior_goals(
   Random_points_on_segment_2<ateam_geometry::Point, ateam_geometry::PointCreator> linePointCreator;
   linePointCreator line_to_goal(ball, middle_of_our_goal);
   // Get two defenders
-  while (defenders.length() < 2) {
+  while (defenders.length() < num_defenders) {
     candidate_points.reserve(50);
     std::copy_n(line_to_goal, 50, std::back_inserter(candidate_points));
     // Remove any that will cause us to be out of bounds
@@ -99,7 +107,7 @@ std::vector<BehaviorGoal> get_defense_behavior_goals(
             MoveParam(ateam_geometry::PointToEigen(candidate))
           }
           points_around_ball.push_back(go_to_point);
-          if (points_around_ball.length() > 1) {
+          if (points_around_ball.length() > num_defenders - 1) {
             break;
           }
         }
