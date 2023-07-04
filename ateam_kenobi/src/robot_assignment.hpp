49a0a81b6fa4fb3149bf0c28bbab4fe6172f30c7
CollinAvidano <collin.avidano@gmail.com>
2023-07-04 14:05:29 -0400

    actually now building robot assignment

--- ateam_kenobi/src/robot_assignment.hpp
+++ ateam_kenobi/src/robot_assignment.hpp
@@ -12,6 +12,8 @@
 #include <iostream>
 #include <limits>
 
+#include <ateam_geometry/types.hpp>
+
 #include "types/robot.hpp"
 #include "ateam_common/assignment.hpp"
 
@@ -25,17 +27,16 @@ std::unordered_map<size_t, size_t> assign(const std::vector<Robot> & available_r
     goal_positions.size(), std::numeric_limits<double>::max());  // NOT INFINITY
   for (size_t i = 0; i < available_robots.size(); i++) {
     for (size_t j = 0; j < goal_positions.size(); j++) {
-      costs(i, j) = sqrt(cgal::squared_distance(available_robots.at(i).pos(), goal_positions.at(j)));
+      costs(i, j) = sqrt(CGAL::squared_distance(available_robots.at(i).pos, goal_positions.at(j)));
     }
   }
 
   // row to column matching assignments
   std::unordered_map assignment = ateam_common::assignment::optimize_assignment(costs);
 
-  std::map<Robot> original_indexes_map {};
-  for(size_t i = 0; i < assignment.size(); i++) {
-    auto grid_assignment = assignment.at(i);
-    original_indexes_map.emplace_back(available_robots.at(grid_assignment->first).id, grid_assignment->second);
+  std::unordered_map<size_t, size_t> original_indexes_map {};
+  for(auto& grid_assignment :assignment) {
+    original_indexes_map.emplace(available_robots.at(grid_assignment.first).id, grid_assignment.second);
   }
   // Map of original robot indexes (1-16) and what of the n goals each is assigned to
   return original_indexes_map;
