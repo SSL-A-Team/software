ccf0143800379d4ea0ee259ad8cd8345c7171c9d
Matthew Woodward <mwoodward@gatech.edu>
2023-07-05 14:46:01 +0200

    Add AI tree display

---
+++ ateam_ui/src/src/AI.ts
@@ -0,0 +1,15 @@
+import { WorldState } from "@/state"
+
+// Types used for ai data including what it thinks the referee states are and the current intended behavior
+
+
+export class AIState {
+    name: string;
+    description: string;
+
+    constructor() {
+        this.name = "No Play Specified";
+        this.description = '{"No Play Specified":""}'
+    }
+}
+
---
+++ ateam_ui/src/src/components/AIComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-card variant="outlined">
+        <div style="margin: 5px">
+            <AIRecursiveComponent v-for="member of Object.getOwnPropertyNames(getAIDescription)"
+                :key="member"
+                :name="member"
+                :node="getAIDescription[member]"
+                :spacing=5
+                />
+        </div>
+    </v-card>
+</template>
+
+<script lang="ts">
+import { ref, inject } from "vue";
+import { AIState } from "@/AI";
+import { Referee, GameStage, GameCommand} from "@/referee";
+import AIRecursiveComponent from "./AIRecursiveComponent.vue";
+
+export default {
+    inject: ['state'],
+    mounted() {
+        console.dir(this.$el)
+    },
+    computed: {
+        getAIDescription: function() {
+            return JSON.parse(this.state.world.ai.description);
+        }
+    },
+    watch: {
+        getAIState: {
+            handler() {
+            },
+            deep: true
+        },
+    },
+    components: {
+        AIRecursiveComponent
+    }
+}
+</script>
