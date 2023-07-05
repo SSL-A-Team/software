e0f92f76aab36cf248c89c8e0491f6f3d5e6f066
CollinAvidano <collin.avidano@gmail.com>
2023-07-05 12:05:15 -0400

    merge kick

--- ateam_kenobi/src/skills/kick.hpp
+++ ateam_kenobi/src/skills/kick.hpp
@@ -22,6 +22,7 @@
 #define SKILLS__KICK_HPP_
 
 #include <ateam_msgs/msg/robot_motion_command.hpp>
+#include "robot_assignment.hpp"
 
 namespace ateam_kenobi::skills
 {
@@ -34,6 +35,98 @@ ateam_msgs::msg::RobotMotionCommand send_kick_command()
   kick_command.kick_speed = 5;
   return kick_command;
 }
+
+// ateam_msgs::msg::RobotMotionCommand line_kick_command(const World & world, std::vector<Robot> available_robots, ateam_geometry::Point target)
+// {
+//   auto assign_robotsRobot current_robot = available_robots;
+
+//   Eigen::Vector3d current, current_vel, target, target_vel;
+//   current.x() = current_robot.pos.x();
+//   current.y() = current_robot.pos.y();
+//   current.z() = current_robot.theta;
+//   current_vel.x() = current_robot.vel.x();
+//   current_vel.y() = current_robot.vel.y();
+//   current_vel.z() = current_robot.omega;
+
+//   const auto & ball = world.get_unique_ball();
+//   if (!ball.has_value() || ball.value().vel.norm() > 0.05) {
+//     target.x() = current_robot.pos.x();
+//     target.y() = current_robot.pos.y();
+//     target.z() = current_robot.theta;
+//     target_vel.x() = 0;
+//     target_vel.y() = 0;
+//     target_vel.z() = 0;
+//   } else {
+//     // robot to ball check to not collide
+//     Eigen::Vector2d ball_pos = ball.value().pos;
+//     Eigen::Vector2d current_robot_pos = current_robot.pos;
+//     Eigen::Vector2d target_goal = Eigen::Vector2d{-6, 0};
+//     Eigen::Vector2d target_setup_pos = ball_pos + 0.4 * (ball_pos - target_goal).normalized();
+
+//     Eigen::Vector2d robot_to_goal = target_goal - current_robot_pos;
+//     double robot_to_goal_angle = ateam_common::geometry::VectorToAngle(robot_to_goal);
+//     double angle_diff = angles::shortest_angular_distance(
+//       current_robot.theta, robot_to_goal_angle);
+
+//     Eigen::Vector2d robot_to_ball = ball_pos - current_robot_pos;
+//     Eigen::Vector2d ball_to_goal = target_goal - ball_pos;
+
+//     // Aligned means
+//     //  * Ball is directly between the robot and goal
+//     //  * Angle difference between heading and robot to goal is low
+//     //  * Robot is almost stopped
+//     bool is_aligned = ateam_common::geometry::IsVectorAligned(
+//       robot_to_goal, robot_to_ball,
+//       0.01);
+//     is_aligned &= std::abs(angle_diff) < 0.05;
+//     is_aligned &= current_robot.vel.norm() < 0.5;
+
+//     if (!is_aligned) {
+//       double projection = (target_setup_pos - current_robot_pos).dot(
+//         ball_pos - current_robot_pos) /
+//         ((target_setup_pos - current_robot_pos).norm() *
+//         (target_setup_pos - current_robot_pos).norm());
+//       projection = std::max(std::min(projection, 1.0), 0.0);
+//       Eigen::Vector2d ball_projected_on_move_line = projection *
+//         (target_setup_pos - current_robot_pos) + current_robot_pos;
+//       double dist = (ball_projected_on_move_line - ball_pos).norm();
+//       if (dist < 0.1) {
+//         target.x() = target_setup_pos.x();
+//         target.y() = target_setup_pos.y() - 1;
+//         target.z() = atan2(ball_to_goal.y(), ball_to_goal.x());
+//         target_vel.x() = 0;
+//         target_vel.y() = 0;
+//         target_vel.z() = 0;
+//       } else {
+//         target.x() = target_setup_pos.x();
+//         target.y() = target_setup_pos.y();
+//         target.z() = atan2(ball_to_goal.y(), ball_to_goal.x());
+//         target_vel.x() = 0;
+//         target_vel.y() = 0;
+//         target_vel.z() = 0;
+//       }
+//     } else {
+//       // Kick
+//       target.x() = ball_pos.x();
+//       target.y() = ball_pos.y();
+//       target.z() = atan2(ball_to_goal.y(), ball_to_goal.x());
+//       target_vel.x() = 0;
+//       target_vel.y() = 0;
+//       target_vel.z() = 0;
+//     }
+//   }
+
+//   Eigen::Vector3d max_vel{3, 3, 1};  // TODO(jneiger): Set as params
+//   Eigen::Vector3d max_accel{1, 1, 1};
+//   double dt = 0.01;  // TODO(jneiger): Feed this down from above
+//   Trajectory trajectory = TrapezoidalMotionProfile::Generate3d(
+//     current, current_vel, target,
+//     target_vel, max_vel, max_accel,
+//     dt, world.current_time + world.immutable_duration);
+
+//   plan.trajectory = trajectory;
+// }
+
 } // namespace ateam_kenobi::skills
 
 #endif // SKILLS__KICK_HPP_
