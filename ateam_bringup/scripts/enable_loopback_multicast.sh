4fb6032220bcd2b2784fa7b33782c06a900a55d1
Matthew Barulic <matthew.barulic@gmail.com>
2023-07-06 20:19:09 +0200

    Makes multicast loopback script package executable

--- ateam_bringup/scripts/enable_loopback_multicast.sh
+++ ateam_bringup/scripts/enable_loopback_multicast.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 sudo ifconfig lo multicast
 sudo route add 224.5.23.1 lo
 sudo route add 224.5.23.2 lo
+echo "Multicast should now work on the loopback interface."
