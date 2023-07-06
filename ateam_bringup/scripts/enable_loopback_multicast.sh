d51fccfff9350468612b5719cad601019f9bca32
Matthew Barulic <matthew.barulic@gmail.com>
2023-07-06 20:20:44 +0200

    Adds comments to multicast loopback script

--- ateam_bringup/scripts/enable_loopback_multicast.sh
+++ ateam_bringup/scripts/enable_loopback_multicast.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo ifconfig lo multicast
-sudo route add 224.5.23.1 lo
-sudo route add 224.5.23.2 lo
+sudo route add 224.5.23.1 lo  # Game Controller
+sudo route add 224.5.23.2 lo  # SSL Vision
 echo "Multicast should now work on the loopback interface."
