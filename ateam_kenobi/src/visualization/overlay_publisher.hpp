680f759d8316a7720b3c5efd175764b5e389b98e
Matthew Barulic <matthew.barulic@gmail.com>
2023-07-07 15:06:18 +0200

    Change default overlay lifetime to 200

--- ateam_kenobi/src/visualization/overlay_publisher.hpp
+++ ateam_kenobi/src/visualization/overlay_publisher.hpp
@@ -17,7 +17,7 @@ public:
   void drawLine(
     const std::string & name, const std::vector<ateam_geometry::Point> & points,
     const std::string & color = "white", const uint8_t stroke_width = 5,
-    const uint32_t lifetime = 100);
+    const uint32_t lifetime = kDefaultLifetime);
 
   void drawCircle(
     const std::string & name, const ateam_geometry::Circle & circle,
@@ -29,12 +29,12 @@ public:
     const std::string & name, const ateam_geometry::Polygon & polygon,
     const std::string & stroke_color = "white",
     const std::string & fill_color = "#FFFFFF7F", const uint8_t stroke_width = 5,
-    const uint32_t lifetime = 100);
+    const uint32_t lifetime = kDefaultLifetime);
 
   void drawText(
     const std::string & name, const std::string & text, const ateam_geometry::Point & position,
     const std::string & color = "white", const uint8_t font_size = 12,
-    const uint32_t lifetime = 100);
+    const uint32_t lifetime = kDefaultLifetime);
 
   /**
    * @brief Publishes and clears the current buffer of overlays.
@@ -43,6 +43,7 @@ public:
   void publishOverlays();
 
 private:
+  static const uint32_t kDefaultLifetime = 200;
   const std::string ns;
   ateam_msgs::msg::OverlayArray overlays_;
   rclcpp::Publisher<ateam_msgs::msg::OverlayArray>::SharedPtr publisher_;
