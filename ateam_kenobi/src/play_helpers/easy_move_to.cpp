afcca4fa6f50646811e8b219aea08231b260e9e4
H.M. Murdock <rcssl.ateam@gmail.com>
2023-07-08 15:55:37 +0200

    Adds missing C++ copyright messages

--- ateam_kenobi/src/play_helpers/easy_move_to.cpp
+++ ateam_kenobi/src/play_helpers/easy_move_to.cpp
@@ -1,3 +1,24 @@
+// Copyright 2021 A Team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
 #include "play_helpers/easy_move_to.hpp"
 #include "easy_move_to.hpp"
 #include <chrono>
@@ -20,7 +41,8 @@ EasyMoveTo::EasyMoveTo(visualization::OverlayPublisher & overlay_publisher)
 : instance_name_("EasyMoveToViz" + std::to_string(instance_index_)),
   overlay_publisher_(&overlay_publisher)
 {
-  std::cerr << "EasyMoveTo instance " << instance_index_ << '\n';
+  std::cerr << "EasyMoveTo instance " << instance_index_ << '
+';
   instance_index_++;
 }
 
@@ -67,7 +89,8 @@ void EasyMoveTo::no_face() {
 
 void EasyMoveTo::setMaxVelocity(double velocity) {
   if (velocity > 3.0) {
-    std::cerr << "UNREASONABLY LARGE VELOCITY GIVEN TO SET MAX VELOCITY\n";
+    std::cerr << "UNREASONABLY LARGE VELOCITY GIVEN TO SET MAX VELOCITY
+";
     return;
   }
   motion_controller_.v_max = velocity;
@@ -75,7 +98,8 @@ void EasyMoveTo::setMaxVelocity(double velocity) {
 
 void EasyMoveTo::setMaxAngularVelocity(double velocity) {
   if (velocity > 6.5) {
-    std::cerr << "UNREASONABLY LARGE VELOCITY GIVEN TO SET MAX ANGULAR VELOCITY\n";
+    std::cerr << "UNREASONABLY LARGE VELOCITY GIVEN TO SET MAX ANGULAR VELOCITY
+";
     return;
   }
   motion_controller_.t_max = velocity;
