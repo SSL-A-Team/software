3c54627d96d75db7eca4691b5d4ed54cb3d8bf41
CollinAvidano <collin.avidano@gmail.com>
2023-07-19 01:47:54 -0400

    just hardcoded it because trying to import it with glisfy or as a string from a file was so painful it was not worth it

--- ateam_ui/src/src/overlay.ts
+++ ateam_ui/src/src/overlay.ts
@@ -1,8 +1,7 @@
 import { RenderConfig } from "@/state"
 import { Field } from "@/field"
-import * as PIXI from "pixi.js";
-import { Texture } from "pixi.js";
-import * as glsify from "glslify";
+import * as PIXI from "pixi.js"
+import { Texture } from "pixi.js"
 
 // Overlay Types
 export class Overlay {
@@ -37,16 +36,47 @@ export class Overlay {
         if (this.lifetime) {
             this.lifetime_end = Date.now() + this.lifetime;
         }
+        let frag_src = `
+            vec4 jet (float x) {
+                const float e0 = 0.0;
+                const vec4 v0 = vec4(0,0,0.5137254901960784,1);
+                const float e1 = 0.125;
+                const vec4 v1 = vec4(0,0.23529411764705882,0.6666666666666666,1);
+                const float e2 = 0.375;
+                const vec4 v2 = vec4(0.0196078431372549,1,1,1);
+                const float e3 = 0.625;
+                const vec4 v3 = vec4(1,1,0,1);
+                const float e4 = 0.875;
+                const vec4 v4 = vec4(0.9803921568627451,0,0,1);
+                const float e5 = 1.0;
+                const vec4 v5 = vec4(0.5019607843137255,0,0,1);
+                float a0 = smoothstep(e0,e1,x);
+                float a1 = smoothstep(e1,e2,x);
+                float a2 = smoothstep(e2,e3,x);
+                float a3 = smoothstep(e3,e4,x);
+                float a4 = smoothstep(e4,e5,x);
+                return max(mix(v0,v1,a0)*step(e0,x)*step(x,e1),
+                    max(mix(v1,v2,a1)*step(e1,x)*step(x,e2),
+                    max(mix(v2,v3,a2)*step(e2,x)*step(x,e3),
+                    max(mix(v3,v4,a3)*step(e3,x)*step(x,e4),mix(v4,v5,a4)*step(e4,x)*step(x,e5)
+                ))));
+            }
 
-          let frag_src = `
-          varying vec2 vTextureCoord;
-          uniform sampler2D uSample;
-          #pragma glslify: cmap = require('glsl-colormap/jet')
+            varying vec2 vTextureCoord;
+            uniform sampler2D uSample;
+            uniform sampler2D uTexture;
 
-          void main(void) {
-            gl_FragColor = cmap(texture2D(uSample, vTextureCoord));
-          }
+            void main () {
+                vec4 dst = jet(texture2D(uTexture, vTextureCoord).r);
+                dst.a = 0.1;
+
+                vec4 src = texture2D(uSample, vTextureCoord);
+                float final_alpha = src.a + dst.a * (1.0 - src.a);
+                gl_FragColor = vec4(
+                    (src.rgb * src.a + dst.rgb * dst.a * (1.0 - src.a)) / final_alpha, final_alpha);
+            }
         `
+
         this.heat_filter = new PIXI.Filter("", frag_src);
 
     }
@@ -144,7 +174,8 @@ export class Overlay {
                 let heat_texture = this.array_to_pixi_texture(this.mesh, this.mesh_alpha)
                 // console.log(heat_texture.valid);
                 if (heat_texture.valid) {
-                  this.heat_filter.uniforms.uSample = heat_texture;
+                  this.heat_filter.uniforms.uTexture = heat_texture;
+                //   this.heat_filter.uniforms.uSample = heat_texture;
 
                   // Filled black rectangle this is mapped to
                   graphic.beginFill('Black');
