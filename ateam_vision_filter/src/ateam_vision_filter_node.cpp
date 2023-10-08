f39056a05be2c4c0ced66541d6612b7cb1b3c7d6
Christian Clark <christianclark687@gmail.com>
2023-10-08 17:29:10 -0400

    Changed references to game state and team color listeners

--- ateam_vision_filter/src/ateam_vision_filter_node.cpp
+++ ateam_vision_filter/src/ateam_vision_filter_node.cpp
@@ -26,7 +26,7 @@
 
 #include <ateam_common/topic_names.hpp>
 #include <ateam_common/indexed_topic_helpers.hpp>
-#include <ateam_common/team_info_listener.hpp>
+#include <ateam_common/game_controller_listener.hpp>
 
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
@@ -44,7 +44,7 @@ class VisionFilterNode : public rclcpp::Node
 public:
   explicit VisionFilterNode(const rclcpp::NodeOptions & options)
   : rclcpp::Node("ateam_vision_filter", options),
-    info_listener_(*this)
+    gc_listener_(*this)
   {
     timer_ = create_wall_timer(10ms, std::bind(&VisionFilterNode::timer_callback, this));
 
@@ -93,7 +93,7 @@ public:
 
     // Our field convention is we should always been on the negative half.
     // So if this is positive for our team we should invert coords
-    if (info_listener_.GetTeamSide() == ateam_common::TeamInfoListener::TeamSide::PositiveHalf) {
+    if (gc_listener_.GetTeamSide() == ateam_common::TeamSide::PositiveHalf) {
       camera_measurement.invert();
       message_conversions::invert_field_info(field_msg);
     }
@@ -147,7 +147,7 @@ private:
   rclcpp::Publisher<ateam_msgs::msg::FieldInfo>::SharedPtr field_publisher_;
   rclcpp::Subscription<ssl_league_msgs::msg::VisionWrapper>::SharedPtr ssl_vision_subscription_;
   rclcpp::Subscription<ssl_league_msgs::msg::VisionWrapper>::SharedPtr ssl_vision_subs_;
-  ateam_common::TeamInfoListener info_listener_;
+  ateam_common::GameControllerListener gc_listener_;
 
   std::mutex world_mutex_;
   World world_;
