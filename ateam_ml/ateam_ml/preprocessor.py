630d085e1de70bfb5b22a46d024da9e224c604e4
CollinAvidano <collin.avidano@gmail.com>
2023-10-11 00:11:15 -0400

    converted this to just glob rather than be a node

--- ateam_ml/ateam_ml/preprocessor.py
+++ ateam_ml/ateam_ml/preprocessor.py
@@ -1,81 +1,72 @@
 import os
 import shutil
+import glob
 import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
-
-from rclpy.node import Node
-from rclpy.serialization import deserialize_message
-from std_msgs.msg import String
-from rosidl_runtime_py.utilities import get_message
-from rosidl_runtime_py import message_to_csv
-from rosidl_runtime_py import message_to_yaml
-from rosidl_runtime_py import message_to_ordereddict
-
-from rclpy.parameter import Parameter
+import argparse
+import numpy as np
 
 from IPython.display import display
 
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
-import rosbag2_py
-
-class Preprocessor(Node):
+class Preprocessor():
     def __init__(self):
-        super().__init__("Preprocessor", automatically_declare_parameters_from_overrides=True)
-        self.reader = rosbag2_py.SequentialReader()
+        parser = argparse.ArgumentParser(
+            prog='ProgramName',
+            description='What the program does',
+            epilog='Text at the bottom of help')
 
-        param_bag_path = self.get_parameter('bag_path').value
-        print(f"Path for bag {param_bag_path}")
-        bag_dir = os.path.dirname(param_bag_path)
+        parser.add_argument('csv_dir')
+        parser.add_argument('output_csv_filepath')
 
-        storage_options = rosbag2_py._storage.StorageOptions(
-            uri=param_bag_path,
-            storage_id='')
-        converter_options = rosbag2_py._storage.ConverterOptions('', '')
-        self.reader.open(storage_options, converter_options)
-
-        topics_meta = self.reader.get_all_topics_and_types()
-        topics_table = {topic_meta.name : get_message(topic_meta.type) for topic_meta in topics_meta}
-
-        excluded_topic_list = ['vision_messages', 'uiclient_count', 'rosout', 'vision_messages', 'ui_connected_clients.csv', 'parameter_events']
-        topic_list = [topic for topic in topics_table if topic not in excluded_topic_list]
+        args = parser.parse_args()
 
         # I dont know why I didnt just do a glob of the *.csvs on path...
 
-        csv_dir = os.path.join(bag_dir, "csvs")
-
         mainframe = pd.DataFrame()
+        topic_csvs = glob.glob(args.csv_dir + "/*.csv")
+        # excluded_topic_list = ['client_count', 'vision_messages', 'uiclient_count', 'rosout', 'vision_messages', 'ui_connected_clients.csv', 'parameter_events']
+        # topic_list = [topic for topic in topics_table if topic not in excluded_topic_list]
+        for csv in topic_csvs:
+            topic = os.path.splitext(os.path.basename(csv))[0]
+            print(topic)
+            # dataframe = pd.read_csv(csv)
+            dataframe = pd.DataFrame()
 
-        for topic in topic_list:
-            csv_path = os.path.join(csv_dir, topic + ".csv")
-            dataframe = pd.read_csv(csv_path)
-            dataframe = dataframe.dropna(axis='index', how='any')
+            for chunk in pd.read_csv(csv, chunksize=100000, low_memory=False):
+                dataframe = pd.concat([dataframe, chunk])
+            # dataframe = dataframe.dropna(axis='index', how='any')
             dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'])
+            dataframe = dataframe[dataframe['timestamp'] > np.datetime64(1*(10**9), 'ns')]
             dataframe = dataframe.set_index('timestamp')
             dataframe = dataframe.add_prefix(f'{topic}.')
 
             mainframe = pd.concat([mainframe, dataframe], axis=1, join='outer')
 
         print("final mainframe:\n ", mainframe)
+        # mainframe.to_csv(args.output_csv_filepath)
 
         # gives a good idea of what you can reasonably interpolate
-        times = pd.DataFrame({"times": mainframe.index.values.astype(float)})
+        times = pd.DataFrame({"times": mainframe.index.values.astype(np.ulonglong)})
+        print("range: ", (times.max() - times.min()) / 10**9)
         diffs_in_seconds = (times['times'].diff() / 10**9)
         print(diffs_in_seconds.describe())
 
+
         upsampled = mainframe.resample(pd.Timedelta('1milli')).mean()
-        # upsampled = mainframe.resample(pd.Timedelta('10milli')).mean()
+        # upsampled.to_csv(args.output_csv_filepath)
+
 
         # interpolated_frame = upsampled.interpolate(method='linear')
         # interpolated_frame = upsampled.interpolate(method='barycentric') # this one takes a small enternity
+
         interpolated_frame = upsampled.interpolate(method='akima')
-
-        interpolated_frame = interpolated_frame.dropna(axis='index', how='any')
-
         print("interpolated_frame:\n ", interpolated_frame)
-        # print(interpolated_frame.count())
+
+        interpolated_frame.to_csv(args.output_csv_filepath)
 
 
         # comparison of pre interpolation to after
@@ -85,22 +76,23 @@ class Preprocessor(Node):
         # plt.show()
 
 
-        tsne = TSNE(n_components=4, verbose=1, perplexity=40, n_iter=300)
-        tsne_results = tsne.fit_transform(interpolated_frame.values)
 
-        interpolated_frame['tsne-2d-one'] = tsne_results[:,0]
-        interpolated_frame['tsne-2d-two'] = tsne_results[:,1]
+        # tsne = TSNE(n_components=4, verbose=1, perplexity=40, n_iter=300)
+        # tsne_results = tsne.fit_transform(interpolated_frame.values)
 
-        plt.figure(figsize=(16,10))
-        sn.scatterplot(
-            x="tsne-2d-one", y="tsne-2d-two",
-            # hue="y",
-            palette=sn.color_palette("hls", 10),
-            data=interpolated_frame,
-            legend="full",
-            alpha=0.3
-        )
-        plt.show()
+        # interpolated_frame['tsne-2d-one'] = tsne_results[:,0]
+        # interpolated_frame['tsne-2d-two'] = tsne_results[:,1]
+
+        # plt.figure(figsize=(16,10))
+        # sn.scatterplot(
+        #     x="tsne-2d-one", y="tsne-2d-two",
+        #     # hue="y",
+        #     palette=sn.color_palette("hls", 10),
+        #     data=interpolated_frame,
+        #     legend="full",
+        #     alpha=0.3
+        # )
+        # plt.show()
 
 
         # ax = plt.figure(figsize=(16,10)).gca(projection='3d')
@@ -117,9 +109,7 @@ class Preprocessor(Node):
         # plt.show()
 
 def main(args=None):
-    rclpy.init(args=args)
     node = Preprocessor()
-    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
