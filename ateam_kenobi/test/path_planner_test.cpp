94eb0f43536d4420c903e6f826489724b5c0c2af
Christian Clark <christianclark687@gmail.com>
2023-11-25 18:03:26 -0500

    Untested path planner tests

--- ateam_kenobi/test/path_planner_test.cpp
+++ ateam_kenobi/test/path_planner_test.cpp
@@ -19,6 +19,7 @@
 // THE SOFTWARE.
 #include "path_planner.hpp"
 #include "types/world.hpp"
+#include "types/robot.hpp"
 
 #include <chrono>
 
@@ -27,7 +28,7 @@
 
 namespace ateam_kenobi {
     /*
-    Fixture for path test setup
+    *   Fixture for path test setup
     */
     class GetPathTest : public ::testing::Test {
         protected:
@@ -40,12 +41,16 @@ namespace ateam_kenobi {
 
             GetPathTest() {}
             virtual ~GetPathTest(){}
+
+            /**
+             * Setup function for path planning tests. Does the following:
+             * Set mock field message based on definitions in section 2 of SSL rule book:
+             * https://robocup-ssl.github.io/ssl-rules/sslrules.html#_field_setup
+             */
             virtual void SetUp() override {
-                // Mock field definition
-                // Should I move this?
-                world.field.field_length = 0;
-                world.field.field_width = 0;
-                world.field.boundary_width = 0;
+                world.field.field_length = 9;
+                world.field.field_width = 6;
+                world.field.boundary_width = 0.01;
                 world.ball.pos = ateam_geometry::Point(50, 50);
                 start_time = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now());
             }
@@ -54,6 +59,10 @@ namespace ateam_kenobi {
             }
     };
 
+    /*
+    *   Test whether we can make a basic path between two points
+    *   without any weird stuff happening (adding more than 1 point or splitting).
+    */
     TEST_F(GetPathTest, StraightPath){
         const auto start = ateam_geometry::Point(0, 0);
         const auto end = ateam_geometry::Point(1, 1);
@@ -66,6 +75,10 @@ namespace ateam_kenobi {
         EXPECT_LT(end_time - start_time, planner_options.search_time_limit + allowed_extra_time);
     }
 
+    /*
+    *   Test whether we can make a path around an obstacle without
+    *   any weird stuff happening (going through the obstacle, not making a path).
+    */
     TEST_F(GetPathTest, PathWithSingleObstacle){
         const auto start = ateam_geometry::Point(0, 0);
         const auto end = ateam_geometry::Point(3, 3);
@@ -80,6 +93,10 @@ namespace ateam_kenobi {
         EXPECT_LT(end_time - start_time, planner_options.search_time_limit + allowed_extra_time);
     }
 
+    /*
+    *   Test whether we can make a path around multiple obstacles without any weird stuff happening
+    *   (not finishing in time, going through the obstacles).
+    */
     TEST_F(GetPathTest, PathWithMultipleObstacles){
         const auto start = ateam_geometry::Point(0, 0);
         const auto end = ateam_geometry::Point(3, 3);
@@ -96,17 +113,60 @@ namespace ateam_kenobi {
         EXPECT_LT(end_time - start_time, planner_options.search_time_limit + allowed_extra_time);
     }
 
-    // Note: this will need a mock message/set of constants for the field boundaries
-    TEST_F(GetPathTest, PathWithFieldBoundaries){}
+    /*
+    *   Test whether an out of bounds point correctly is identified as invalid.
+    */
+    TEST_F(GetPathTest, OutOfBounds){
+        const auto invalid_point = ateam_geometry::Point(10,10);
+        bool is_valid = path_planner.isStateValid(invalid_point, world, obstacles, planner_options);
+        EXPECT_FALSE(is_valid);
+    }
 
-    // We'll need a mock, use the same one as above
-    TEST(CheckValidState, ValidState){}
+    /*
+    *   Test whether a point inside of an obstacle correctly is identified as invalid.
+    */
+    TEST_F(GetPathTest, InObstacle){
+        const auto obstacle = ateam_geometry::makeCircle(ateam_geometry::Point(1, 1), 1);
+        obstacles.push_back(obstacle);
+        const auto invalid_point = ateam_geometry::Point(10,10);
+        bool is_valid = path_planner.isStateValid(invalid_point, world, obstacles, planner_options);
+        EXPECT_FALSE(is_valid);
+    }
 
-    TEST(CheckValidState, OutOfBounds){}
+    /*
+    *   Test whether a robot in the world correctly gets added to the list of obstacles.
+    */
+    TEST_F(GetPathTest, CreateObstaclesFromRobots){
+        const auto robot = std::optional<Robot>;
+        robot.value().id = 0;
+        robot.value().pos = ateam_geometry::Point(0,0);
+        robot.value().vel = 0;
+        world.our_robots[0].value() = robot.value();
+        const auto start_pos = ateam_geometry::Point(2,2);
+        path_planner.addRobotsToObstacles(world, start_pos, obstacles);
+        EXPECT_FALSE(obstacles.empty());
+        EXPECT_EQ(1, obstacles.size());
+        EXPECT_EQ(ateam_geometry::makeCircle(robot.value().pos, 0.09), obstacles.front());
+    }
 
-    TEST(CheckValidState, Obstacle){}
-
-    TEST(Obstacles, CreateObstaclesFromRobots){}
-
-    TEST(Obstacles, GetDefaultObstacles){}
+    /*
+    *   Test whether we correctly add unallowed play areas to the list of default obstacles.
+    */
+    TEST_F(GetPathTest, GetDefaultObstacles){
+        path_planner.addDefaultObstacles(world, obstacles);
+        EXPECT_EQ(2, obstacles.size());
+        EXPECT_FALSE(obstacles.empty());
+        EXPECT_EQ(ateam_geometry::Rectangle(
+            ateam_geometry::Point(-world.field.field_length / 2, world.field.goal_width),
+            ateam_geometry::Point(
+                -1 * (world.field.field_length / 2) + world.field.goal_width,
+                -world.field.goal_width)
+        ), obstacles.front());
+        EXPECT_EQ(ateam_geometry::Rectangle(
+        ateam_geometry::Point((world.field.field_length / 2), world.field.goal_width),
+        ateam_geometry::Point(
+            (world.field.field_length / 2) + world.field.goal_width,
+            -world.field.goal_width)
+        ), obstacles.back());
+    }
 } // namespace ateam_kenobi
