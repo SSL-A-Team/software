dfaa496ca7a44a38af60edd8b682bcd369514d5a
Matthew Barulic <matthew.barulic@gmail.com>
2024-01-23 01:01:45 -0500

    Rewriting InPlayEval to fix bugs.

---
+++ ateam_kenobi/src/in_play_eval.cpp
@@ -0,0 +1,109 @@
+// Copyright 2024 A Team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "in_play_eval.hpp"
+
+#include <cmath>
+#include <CGAL/squared_distance_2.h>
+
+namespace ateam_kenobi
+{
+
+void InPlayEval::Update(World & world)
+{
+  // Force start always puts the ball in play
+  if (world.referee_info.running_command == ateam_common::GameCommand::ForceStart) {
+    in_play_ = true;
+  }
+  // Stop puts the ball out of play
+  else if (world.referee_info.running_command == ateam_common::GameCommand::Stop) {
+    in_play_ = false;
+    ball_start_pos_.reset();
+  }
+  // If game's not stopped, we're going to have to pay attention to in play conditions
+  else if (world.referee_info.running_command != ateam_common::GameCommand::Stop &&
+    world.referee_info.running_command != ateam_common::GameCommand::Halt)
+  {
+    // If this is our first frame out of stop, establish starting conditions
+    if (!ball_start_pos_) {
+      ball_start_pos_ = world.ball.pos;
+      SetTimeout(world);
+      SetDistanceThreshold(world);
+    }
+
+    // If timeout passed, ball's in play
+    if (timeout_time_.time_since_epoch().count() != 0 &&
+      std::chrono::steady_clock::now() > timeout_time_)
+    {
+      in_play_ = true;
+    }
+
+    // If ball moved far enough, it's in play
+    if (ball_start_pos_ &&
+      std::sqrt(
+        CGAL::squared_distance(
+          world.ball.pos,
+          ball_start_pos_.value())) > ball_moved_threshold_)
+    {
+      in_play_ = true;
+    }
+  }
+
+  prev_game_command_ = world.referee_info.running_command;
+}
+
+void InPlayEval::SetTimeout(const World & world)
+{
+  switch (world.referee_info.running_command) {
+    case ateam_common::GameCommand::PrepareKickoffOurs:
+    case ateam_common::GameCommand::PrepareKickoffTheirs:
+      timeout_time_ = std::chrono::steady_clock::now() + std::chrono::seconds(10);
+      break;
+    case ateam_common::GameCommand::DirectFreeOurs:
+    case ateam_common::GameCommand::DirectFreeTheirs:
+      // Division A
+      // timeout_time_ = std::chrono::steady_clock::now() + std::chrono::seconds(5);
+      // Division B
+      timeout_time_ = std::chrono::steady_clock::now() + std::chrono::seconds(10);
+      break;
+    default:
+      timeout_time_ = {}; // No timeout
+      break;
+  }
+}
+
+void InPlayEval::SetDistanceThreshold(const World & world)
+{
+  switch (world.referee_info.running_command) {
+    case ateam_common::GameCommand::PrepareKickoffOurs:
+    case ateam_common::GameCommand::PrepareKickoffTheirs:
+    case ateam_common::GameCommand::DirectFreeOurs:
+    case ateam_common::GameCommand::DirectFreeTheirs:
+    case ateam_common::GameCommand::PreparePenaltyOurs:
+    case ateam_common::GameCommand::PreparePenaltyTheirs:
+      ball_moved_threshold_ = 0.05;
+      break;
+    default:
+      ball_moved_threshold_ = std::numeric_limits<double>::infinity();
+      break;
+  }
+}
+
+}  // namespace ateam_kenobi
