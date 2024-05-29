f72d23714953969d01930fe993e57c4107bf3136
Christian Clark <christianclark687@gmail.com>
2024-05-28 22:51:36 -0400

    Hotfix for unequal number of tasks and robots

--- ateam_common/src/km_assignment.cpp
+++ ateam_common/src/km_assignment.cpp
@@ -369,6 +369,9 @@ std::vector<int> km_assignment(
 
   // Return our perfect matching
   // based on our feasible labeling
+  if (xy.size() >= cost_matrix.rows()) {
+    xy.resize(cost_matrix.rows());
+  }
   return xy;
 }
 
