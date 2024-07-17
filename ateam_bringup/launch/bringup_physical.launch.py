0610d1e3137f537a7dab16ef099b7ce982f75570
H.M. Murdock <rcssl.ateam@gmail.com>
2024-07-17 08:49:11 -0400

    Updates IPs for comp.

--- ateam_bringup/launch/bringup_physical.launch.py
+++ ateam_bringup/launch/bringup_physical.launch.py
@@ -44,7 +44,7 @@ def generate_launch_description():
         # DeclareLaunchArgument("radio_interface_address", default_value="172.16.1.10"),
 
         # Competition IPs
-        DeclareLaunchArgument("vision_interface_address", default_value="10.193.15.132"),
+        DeclareLaunchArgument("vision_interface_address", default_value="192.168.1.40"),
         DeclareLaunchArgument("radio_interface_address", default_value="172.16.1.10"),
         DeclareLaunchArgument("team_name", default_value="A-Team"),
         DeclareLaunchArgument("use_local_gc", default_value="False"),
@@ -60,8 +60,8 @@ def generate_launch_description():
             condition=UnlessCondition(LaunchConfiguration("use_local_gc")),
             scoped=False,
             actions=[
-                DeclareLaunchArgument("gc_interface_address", default_value="10.193.15.132"),
-                DeclareLaunchArgument("gc_server_address", default_value="10.193.12.10"),
+                DeclareLaunchArgument("gc_interface_address", default_value="192.168.1.40"),
+                DeclareLaunchArgument("gc_server_address", default_value="192.168.0.114"),
             ]),
 
         IncludeLaunchDescription(
