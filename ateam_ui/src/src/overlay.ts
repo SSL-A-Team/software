a4710b804cd778d751e14e2e5aa11ce1a568f0f1
Matthew Barulic <matthew.barulic@gmail.com>
2025-01-13 21:49:54 -0500

    Refactors overlay implementation to initialize heatmap filter only for heatmap overlays.

--- ateam_ui/src/src/overlay.ts
+++ ateam_ui/src/src/overlay.ts
@@ -59,53 +59,9 @@ export class Overlay {
             this.lifetime_end = Date.now() + this.lifetime;
         }
 
-        let cmap_src = `
-            // jet cmap
-            vec4 cmap (float x) {
-                const float e0 = 0.0;
-                const vec4 v0 = vec4(0,0,0.5137254901960784,1);
-                const float e1 = 0.125;
-                const vec4 v1 = vec4(0,0.23529411764705882,0.6666666666666666,1);
-                const float e2 = 0.375;
-                const vec4 v2 = vec4(0.0196078431372549,1,1,1);
-                const float e3 = 0.625;
-                const vec4 v3 = vec4(1,1,0,1);
-                const float e4 = 0.875;
-                const vec4 v4 = vec4(0.9803921568627451,0,0,1);
-                const float e5 = 1.0;
-                const vec4 v5 = vec4(0.5019607843137255,0,0,1);
-                float a0 = smoothstep(e0,e1,x);
-                float a1 = smoothstep(e1,e2,x);
-                float a2 = smoothstep(e2,e3,x);
-                float a3 = smoothstep(e3,e4,x);
-                float a4 = smoothstep(e4,e5,x);
-                return max(mix(v0,v1,a0)*step(e0,x)*step(x,e1),
-                    max(mix(v1,v2,a1)*step(e1,x)*step(x,e2),
-                    max(mix(v2,v3,a2)*step(e2,x)*step(x,e3),
-                    max(mix(v3,v4,a3)*step(e3,x)*step(x,e4),mix(v4,v5,a4)*step(e4,x)*step(x,e5)
-                ))));
-            }
-        `
-
-        let frag_src = cmap_src + `
-            varying vec2 vTextureCoord;
-            uniform sampler2D uSample;
-            uniform sampler2D uTexture;
-
-            void main () {
-                vec4 dst = cmap(texture2D(uTexture, vTextureCoord).r);
-                dst.a = 0.1; // alpha control on heatmap as a whole
-
-                vec4 src = texture2D(uSample, vTextureCoord);
-                float src_a = src.a / 255.0;
-                float final_alpha = src_a + dst.a * (1.0 - src_a);
-                gl_FragColor = vec4(
-                    (src.rgb * src_a + dst.rgb * dst.a * (1.0 - src_a)) / final_alpha, final_alpha);
-            }
-        `
-
-        this.heatmap_filter = new PIXI.Filter("", frag_src);
-        this.heatmap_filter.uniforms.uTexture = this.createHeatmapTexture();
+        if(this.type == OverlayType.Heatmap) {
+            this.initializeHeatmapFilter();
+        }
     }
 
     /**
@@ -242,7 +198,7 @@ export class Overlay {
         container.addChild(graphic);
     }
 
-    createHeatmapTexture() {
+    initializeHeatmapFilter() {
         const bytes_per_pixel = 4;
         const num_pixels = this.heatmap_resolution_width * this.heatmap_resolution_height;
         let buffer = new Uint8Array(num_pixels * bytes_per_pixel);
@@ -276,6 +232,52 @@ export class Overlay {
             buffer[dst_index + 3] = alpha;
         }
 
-        return PIXI.Texture.fromBuffer(buffer, this.heatmap_resolution_width, this.heatmap_resolution_height, {scaleMode: PIXI.SCALE_MODES.LINEAR});
+        let cmap_src = `
+            // jet cmap
+            vec4 cmap (float x) {
+                const float e0 = 0.0;
+                const vec4 v0 = vec4(0,0,0.5137254901960784,1);
+                const float e1 = 0.125;
+                const vec4 v1 = vec4(0,0.23529411764705882,0.6666666666666666,1);
+                const float e2 = 0.375;
+                const vec4 v2 = vec4(0.0196078431372549,1,1,1);
+                const float e3 = 0.625;
+                const vec4 v3 = vec4(1,1,0,1);
+                const float e4 = 0.875;
+                const vec4 v4 = vec4(0.9803921568627451,0,0,1);
+                const float e5 = 1.0;
+                const vec4 v5 = vec4(0.5019607843137255,0,0,1);
+                float a0 = smoothstep(e0,e1,x);
+                float a1 = smoothstep(e1,e2,x);
+                float a2 = smoothstep(e2,e3,x);
+                float a3 = smoothstep(e3,e4,x);
+                float a4 = smoothstep(e4,e5,x);
+                return max(mix(v0,v1,a0)*step(e0,x)*step(x,e1),
+                    max(mix(v1,v2,a1)*step(e1,x)*step(x,e2),
+                    max(mix(v2,v3,a2)*step(e2,x)*step(x,e3),
+                    max(mix(v3,v4,a3)*step(e3,x)*step(x,e4),mix(v4,v5,a4)*step(e4,x)*step(x,e5)
+                ))));
+            }
+        `
+
+        let frag_src = cmap_src + `
+            varying vec2 vTextureCoord;
+            uniform sampler2D uSample;
+            uniform sampler2D uTexture;
+
+            void main () {
+                vec4 dst = cmap(texture2D(uTexture, vTextureCoord).r);
+                dst.a = 0.1; // alpha control on heatmap as a whole
+
+                vec4 src = texture2D(uSample, vTextureCoord);
+                float src_a = src.a / 255.0;
+                float final_alpha = src_a + dst.a * (1.0 - src_a);
+                gl_FragColor = vec4(
+                    (src.rgb * src_a + dst.rgb * dst.a * (1.0 - src_a)) / final_alpha, final_alpha);
+            }
+        `
+
+        this.heatmap_filter = new PIXI.Filter("", frag_src);
+        this.heatmap_filter.uniforms.uTexture = PIXI.Texture.fromBuffer(buffer, this.heatmap_resolution_width, this.heatmap_resolution_height, {scaleMode: PIXI.SCALE_MODES.LINEAR});
     }
 }
