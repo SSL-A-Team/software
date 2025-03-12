510ae4c932a94c20f4843f8700961be2576f8e6b
Christian Clark <christianclark687@gmail.com>
2025-03-11 22:23:22 -0400

    Distance down field

--- ateam_spatial/src/layers/distance_down_field.cu
+++ ateam_spatial/src/layers/distance_down_field.cu
@@ -7,5 +7,20 @@ namespace ateam_spatial::layers
 CUDA_HOSTDEV float DistanceDownField(const int x, const FieldDimensions & field_dims, const SpatialSettings & settings) {
   return SpatialToRealX(x, field_dims, settings) + (WorldWidthReal(field_dims) / 2.0);
 }
+
+__global__
+void distanceDownFieldKernel(unsigned char* Pout, unsigned char* Pin, const FieldDimensions & field_dims,
+    const SpatialSettings & settings)
+{
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if (col < width && row < height){
+    int out_coord_array_pos = row * width + col;
+
+    float distance = DistanceDownField(col, field_dims, settings);
+    Pout[out_coord_array_pos]
+  }
+}
   
 } // namespace ateam_spatial::layers
