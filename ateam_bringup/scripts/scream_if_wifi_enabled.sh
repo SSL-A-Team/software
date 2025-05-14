db91c2a41ab48bb0a658129328d0578e411712c8
Matthew Barulic <matthew.barulic@gmail.com>
2025-05-13 22:48:17 -0400

    Makes wifi screamer stop when wifi is turned off.

--- ateam_bringup/scripts/scream_if_wifi_enabled.sh
+++ ateam_bringup/scripts/scream_if_wifi_enabled.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 
-if nmcli radio wifi | grep -q "enabled"; then
-  while true; do
-    echo -e "\e[31mWIFI IS ENABLED! TURN IT OFF!\e[0m" >&2
-    sleep 1
-  done
-else
-  echo "WiFi disabled check passed." >&2
-fi
+while nmcli radio wifi | grep -q "enabled"; do
+  echo -e "\e[31mWIFI IS ENABLED! TURN IT OFF!\e[0m" >&2
+  sleep 1
+done
