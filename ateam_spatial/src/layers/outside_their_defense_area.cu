afa02608db78268fdef69e64839ec56c7bdc68b7
Matthew Barulic <matthew.barulic@gmail.com>
2025-07-16 03:08:01 -0300

    Adds more buffer to defense area for pass targets.

--- ateam_spatial/src/layers/outside_their_defense_area.cu
+++ ateam_spatial/src/layers/outside_their_defense_area.cu
@@ -5,10 +5,10 @@ namespace ateam_spatial::layers
 {
 
 CUDA_HOSTDEV float OutsideTheirDefenseArea(const float x, const float y, const FieldDimensions & field_dims, const SpatialSettings & settings) {
-  const auto robot_radius = 0.09f;
+  const auto robot_diameter = 0.180f;
   const auto pos_x_thresh = field_dims.field_length / 2.0f;
-  const auto neg_x_thresh = (field_dims.field_length / 2.0f) - (field_dims.defense_area_depth + (robot_radius * 1.1f));
-  const auto pos_y_thresh = (field_dims.defense_area_width / 2.0f) + (robot_radius * 1.1f);
+  const auto neg_x_thresh = (field_dims.field_length / 2.0f) - (field_dims.defense_area_depth + (robot_diameter * 3.0f));
+  const auto pos_y_thresh = (field_dims.defense_area_width / 2.0f) + (robot_diameter * 3.0f);
   const auto neg_y_thresh = -pos_y_thresh;
 
   return !((x <= pos_x_thresh) && (x >= neg_x_thresh) && (y <= pos_y_thresh) && (y >= neg_y_thresh));
