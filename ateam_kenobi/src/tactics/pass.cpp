1ba9d6d8a2f7e60633992a538b2ce9d08f4c1f28
Matthew Barulic <matthew.barulic@gmail.com>
2025-07-16 15:07:50 -0300

    Adjusts pass ball model for comp fields

--- ateam_kenobi/src/tactics/pass.cpp
+++ ateam_kenobi/src/tactics/pass.cpp
@@ -118,11 +118,11 @@ bool Pass::isDone()
 double Pass::calculateDefaultKickSpeed(const World & world)
 {
   const auto distance = CGAL::approximate_sqrt(CGAL::squared_distance(world.ball.pos, target_));
-  const auto ball_friction_acceleration = 1.0;
-  const auto max_kick_speed = 5.0;
+  const auto ball_friction_acceleration = 0.4;
+  const auto max_kick_speed = 5.5;
   const auto velocity_at_receiver = 0.2;
   const auto stop_at_receiver_velocity = std::sqrt(2.0 * ball_friction_acceleration * distance);
-  return std::min((velocity_at_receiver + stop_at_receiver_velocity) / 1.0, max_kick_speed);
+  return std::min(velocity_at_receiver + stop_at_receiver_velocity, max_kick_speed);
 }
 
 }  // namespace ateam_kenobi::tactics
