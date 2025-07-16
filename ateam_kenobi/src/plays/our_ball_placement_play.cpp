bbbab5927cf0dbd55d24783c21ba24913c354285
Matthew Woodward <mwoodward@gatech.edu>
2025-07-16 19:21:12 -0300

    Add extract to our ball placement

--- ateam_kenobi/src/plays/our_ball_placement_play.cpp
+++ ateam_kenobi/src/plays/our_ball_placement_play.cpp
@@ -47,7 +47,7 @@ stp::PlayScore OurBallPlacementPlay::getScore(const World & world)
 
 void OurBallPlacementPlay::reset()
 {
-  state_ = State::Passing;
+  state_ = State::Placing;
   pass_tactic_.reset();
   dribble_.reset();
   for (auto & emt : easy_move_tos_) {
@@ -86,15 +86,37 @@ std::array<std::optional<ateam_msgs::msg::RobotMotionCommand>, 16> OurBallPlacem
       placement_point_,
       0.15), "green");
 
+  auto maybe_offset_vector = calculateObstacleOffset(world);
+
+  const bool use_extract = true;
+
   if (state_ != State::Placing &&
     world.ball.visible &&
     ball_dist < 0.12 &&
     ball_speed < 0.04) {
 
     state_ = State::Done;
+
+  // detect ball near goal/walls
+  } else if (use_extract && ball_dist > 0.2 && maybe_offset_vector.has_value()) {
+    approach_point_ = world.ball.pos + maybe_offset_vector.value();
+    state_ = State::Extracting;
+    getOverlays().drawCircle("approach_point", ateam_geometry::makeCircle(approach_point_, 0.025), "#0000FFFF");
   }
 
+  const bool see_ball_clear_of_obstacle = world.ball.visible && !maybe_offset_vector.has_value();
+
   switch (state_) {
+    case State::Extracting:
+      // ball is clear of obstacles
+      if (see_ball_clear_of_obstacle) {
+        state_ = State::Placing;
+        break;
+      }
+
+      getPlayInfo()["State"] = "Extracting";
+      runExtracting(available_robots, world, maybe_motion_commands);
+      break;
     case State::Passing:
       if (pass_tactic_.isDone() ||
         (ball_dist < 1.0 && ball_speed < 0.05) ||
@@ -143,6 +165,7 @@ std::array<std::optional<ateam_msgs::msg::RobotMotionCommand>, 16> OurBallPlacem
 
       ateam_geometry::Point target_position = robot.pos;
       if (ateam_geometry::norm(robot.pos - nearest_point) < 0.6 + kRobotRadius) {
+
         target_position = nearest_point +
           0.7 * ateam_geometry::Vector(std::cos(angle + M_PI / 2), std::sin(angle + M_PI / 2));
 
@@ -168,14 +191,14 @@ std::array<std::optional<ateam_msgs::msg::RobotMotionCommand>, 16> OurBallPlacem
         }
       }
 
+        emt.setTargetPosition(target_position);
+        emt.face_point(world.ball.pos);
+        maybe_motion_commands[robot.id] = emt.runFrame(robot, world);
+
         getPlayInfo()["Robots"][std::to_string(robot.id)] = "MOVING";
       } else {
         getPlayInfo()["Robots"][std::to_string(robot.id)] = "-";
       }
-
-      emt.setTargetPosition(target_position);
-      emt.face_point(world.ball.pos);
-      maybe_motion_commands[robot.id] = emt.runFrame(robot, world);
     }
   }
 
@@ -232,6 +255,92 @@ void OurBallPlacementPlay::runPassing(
   pass_tactic_.runFrame(world, kicker_robot, receiver_robot, kicker_command, receiver_command);
 
   getPlayInfo()["Pass Tactic"] = pass_tactic_.getPlayInfo();
+}
+
+void OurBallPlacementPlay::runExtracting(
+  const std::vector<Robot> & available_robots, const World & world,
+  std::array<std::optional<ateam_msgs::msg::RobotMotionCommand>,
+  16> & motion_commands)
+{
+  const auto approach_point = approach_point_;
+  auto byDistToApproachPoint = [&approach_point](const Robot & lhs, const Robot & rhs) {
+      return CGAL::compare_distance_to_point(approach_point, lhs.pos, rhs.pos) == CGAL::SMALLER;
+    };
+
+  const auto & extract_robot = *std::min_element(
+    available_robots.begin(),
+    available_robots.end(), byDistToApproachPoint);
+
+  getPlayInfo()["Assignments"]["Extractor"] = extract_robot.id;
+
+  auto & emt = easy_move_tos_[extract_robot.id];
+
+  const auto robot_to_ball = (world.ball.pos - extract_robot.pos);
+  const auto ball_to_approach = approach_point_ - world.ball.pos;
+
+  // const double ball_to_approach_angle = std::atan2(ball_to_approach.y(), ball_to_approach.x());
+  const double robot_approach_angle_offset = std::acos((-robot_to_ball * ball_to_approach)
+    / (ateam_geometry::norm(robot_to_ball) * ateam_geometry::norm(ball_to_approach))
+  );
+  const double robot_to_ball_angle = std::atan2(robot_to_ball.y(), robot_to_ball.x());
+
+  const bool robot_near_approach_point = ateam_geometry::norm(
+    extract_robot.pos - approach_point_
+  ) < 0.05;
+  const bool robot_facing_ball = abs(robot_to_ball_angle - extract_robot.theta) < 0.1;
+  const bool robot_near_ball = ateam_geometry::norm(robot_to_ball) <= approach_radius_;
+  const bool robot_already_in_position = robot_near_ball && robot_facing_ball
+    && abs(robot_approach_angle_offset) < 0.3;
+
+  auto motion_command = ateam_msgs::msg::RobotMotionCommand();
+  if (extract_robot.breakbeam_ball_detected_filtered) {
+    getPlayInfo()["ExtractState"] = "extracting ball";
+
+    motion_command.twist_frame = ateam_msgs::msg::RobotMotionCommand::FRAME_BODY;
+    motion_command.twist.linear.x = -0.35;
+    motion_command.twist.linear.y = 0.0;
+
+  } else if (robot_already_in_position || robot_near_approach_point) {
+    getPlayInfo()["ExtractState"] = "capturing ball";
+    MotionOptions motion_options;
+    motion_options.completion_threshold = 0;
+    emt.setMotionOptions(motion_options);
+    path_planning::PlannerOptions planner_options = emt.getPlannerOptions();
+    planner_options.avoid_ball = false;
+    planner_options.footprint_inflation = -0.9 * kRobotRadius;
+    emt.setPlannerOptions(planner_options);
+
+    emt.setMaxAccel(2.0);
+    emt.setMaxDecel(2.0);
+    if (world.ball.visible) {
+      emt.face_point(world.ball.pos);
+
+    // If the ball is occluded we sometimes drive past its previous position and try to turn around
+    // so its better to just keep facing the same direction if we lose track of it
+    } else {
+      emt.face_absolute(extract_robot.theta);
+    }
+    emt.setTargetPosition(world.ball.pos);
+    motion_command = emt.runFrame(extract_robot, world);
+    motion_command.twist_frame = ateam_msgs::msg::RobotMotionCommand::FRAME_BODY;
+    motion_command.twist.linear.x = 0.35;
+    motion_command.twist.linear.y = 0.0;
+  } else {
+    getPlayInfo()["ExtractState"] = "moving to approach point";
+    emt.setMaxAccel(2.0);
+    emt.setMaxDecel(2.0);
+    emt.face_absolute(ateam_geometry::ToHeading(world.ball.pos - approach_point_));
+    emt.setTargetPosition(approach_point_);
+    motion_command = emt.runFrame(extract_robot, world);
+  }
+
+  const bool should_dribble = extract_robot.breakbeam_ball_detected_filtered ||
+    ateam_geometry::norm(robot_to_ball) < 0.5;
+  if (should_dribble) {
+    motion_command.dribbler_speed = 300;
+  }
+
+  motion_commands[extract_robot.id] = motion_command;
 }
 
 void OurBallPlacementPlay::runPlacing(
@@ -313,5 +422,69 @@ void OurBallPlacementPlay::DrawKeepoutArea(
   overlays.drawLine("placement_avoid_neg_line", {neg_segment.source(), neg_segment.target()},
       "Cyan");
 }
+
+std::optional<ateam_geometry::Vector> OurBallPlacementPlay::calculateObstacleOffset(const World & world)
+{
+  double obstacle_offset_x = 0;
+  double obstacle_offset_y = 0;
+
+  // Handle ball inside goal
+  bool is_inside_goal_x = abs(world.ball.pos.x()) > world.field.field_length / 2;
+  bool is_between_goalposts = abs(world.ball.pos.y()) < world.field.goal_width / 2;
+  bool is_inside_goal = is_inside_goal_x && is_between_goalposts;
+
+  if (is_inside_goal) {
+    bool is_near_back_goal_x = abs(world.ball.pos.x()) + ball_distance_from_obstacle_ > (world.field.field_length / 2) + world.field.goal_depth;
+    bool is_near_inside_goalposts_y = abs(world.ball.pos.y()) + ball_distance_from_obstacle_ > world.field.goal_width / 2;
+
+    if (is_near_back_goal_x) {
+      obstacle_offset_x = (world.ball.pos.x() > 0) ? -1.0 : 1.0;
+    }
+    if (is_near_inside_goalposts_y) {
+      obstacle_offset_y = (world.ball.pos.y() > 0) ? -1.0 : 1.0;
+    }
+
+    auto offset_vector = approach_radius_ * ateam_geometry::normalize(ateam_geometry::Vector(obstacle_offset_x, obstacle_offset_y));
+    return std::make_optional(offset_vector);
+  }
+
+  // Handle ball near outside goalposts
+  bool is_near_outside_goalposts_x = abs(world.ball.pos.x()) + ball_distance_from_obstacle_ > world.field.field_length / 2;
+  bool is_near_outside_goalposts_y = abs(world.ball.pos.y()) - ball_distance_from_obstacle_ < (world.field.goal_width / 2) + 0.02;
+  bool is_near_outside_goalposts = is_near_outside_goalposts_x && is_near_outside_goalposts_y;
+
+  if (is_near_outside_goalposts) {
+    if (is_near_outside_goalposts_x) {
+      obstacle_offset_x = (world.ball.pos.x() > 0) ? -1.0 : 1.0;
+    }
+    if (is_near_outside_goalposts_y) {
+      obstacle_offset_y = (world.ball.pos.y() > 0) ? 1.0 : -1.0;
+    }
+
+    auto offset_vector = approach_radius_ * ateam_geometry::normalize(ateam_geometry::Vector(obstacle_offset_x, obstacle_offset_y));
+    return std::make_optional(offset_vector);
+  }
+
+  // Handle ball near walls
+  bool is_near_x_walls =  abs(world.ball.pos.x()) + ball_distance_from_obstacle_ > (world.field.field_length / 2) + world.field.boundary_width;
+  bool is_near_y_walls =  abs(world.ball.pos.y()) + ball_distance_from_obstacle_ > (world.field.field_width / 2) + world.field.boundary_width;
+  bool is_near_wall =  is_near_x_walls || is_near_y_walls;
+
+  if (is_near_wall) {
+    if (is_near_x_walls) {
+      obstacle_offset_x = (world.ball.pos.x() > 0) ? -1.0 : 1.0;
+    }
+    if (is_near_y_walls) {
+      obstacle_offset_y = (world.ball.pos.y() > 0) ? -1.0 : 1.0;
+    }
+
+    auto offset_vector = approach_radius_ * ateam_geometry::normalize(ateam_geometry::Vector(obstacle_offset_x, obstacle_offset_y));
+    return std::make_optional(offset_vector);
+  }
+
+  // No obstacles to account for
+  return std::nullopt;
+}
+
 
 }  // namespace ateam_kenobi::plays
