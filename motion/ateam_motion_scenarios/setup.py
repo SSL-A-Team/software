a54d2f58c23d2806750a638716c7b606d6842e9f
Nicholas Witten <nwitt12@gmail.com>
2026-06-05 02:15:15 -0700

    add ateam_motion_scenarios

---
+++ motion/ateam_motion_scenarios/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'ateam_motion_scenarios'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='A-Team',
+    maintainer_email='ateam@example.com',
+    description='Scripted motion scenarios for profiling robot motion performance.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ball_capture_scenario = ateam_motion_scenarios.ball_capture_scenario:main',
+        ],
+    },
+)
