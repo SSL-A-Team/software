62f7721ba672399d36ec8f9b8c96649936b3c2b9
Matthew Barulic <matthew.barulic@gmail.com>
2026-06-16 23:42:08 -0400

    Adds ateam_controls package

--- motion/ateam_controls_testing/scripts/waypoints_test.py
+++ motion/ateam_controls_testing/scripts/waypoints_test.py
@@ -59,8 +59,8 @@ def publish_waypoint_command(index: int):
     command_msg.pose.y = waypoint[1]
     command_msg.pose.theta = waypoint[2]
     command_msg.kick_request = RobotMotionCommand.KR_DISABLE
-    command_msg.limit_acc_linear = 4.0
-    command_msg.limit_vel_linear = 2.0
+    command_msg.limit_acc_linear = 3.0
+    command_msg.limit_vel_linear = 3.0
     command_msg.limit_acc_angular = 8.0
     command_msg.limit_vel_angular = 4.0
     command_pub.publish(command_msg)
