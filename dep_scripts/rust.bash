c7d79282db65c88ad138c68714439685c2b323f1
Matthew Barulic <matthew.barulic@gmail.com>
2026-06-17 01:00:12 -0400

    Adds ateam_controls package (#410)
    * Adds dependency install scrips.
    
    * Adds ateam_controls package
    
    * Makes actions workflows use new dependency script
    
    * Fixes path typo in actions workflows
    
    * Adds yes flag to apt upgrade
    
    * Adds rosdep update step
    
    * Makes rust installer noninteractive
    
    * Gates sim install behind flag
    
    * Adds rust install environment update step
    
    * Fixes controls library exporting
    
    * Adds GH Actions-native rust install step

---
+++ dep_scripts/rust.bash
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+set -e
+
+if command -v rustc &> /dev/null; then
+    echo "Rust already installed."
+    exit 0
+fi
+
+echo "Installing Rust..."
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+echo "Rust installed."
