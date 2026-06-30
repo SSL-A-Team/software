3ad78040aab801b67f4755db5f3ff99c08d61784
Matthew Barulic <matthew.barulic@gmail.com>
2026-06-30 14:05:57 +0900

    Adds ateam_controls_cpp with stubs for PositionAtT

---
+++ motion/ateam_controls/include/ateam_controls_cpp/predict.hpp
@@ -0,0 +1,80 @@
+// Copyright 2026 A Team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef ATEAM_CONTROLS_CPP__PREDICT_HPP_
+#define ATEAM_CONTROLS_CPP__PREDICT_HPP_
+
+#include <ateam_game_state/robot.hpp>
+#include <ateam_geometry/types.hpp>
+
+namespace ateam_controls_cpp::predict
+{
+
+namespace modes
+{
+struct Off{};
+
+struct EstopBrake{};
+
+struct GlobalPosition
+{
+  float global_x;
+  float global_y;
+  float global_theta;
+  float max_linear_vel;
+  float max_angular_vel;
+  float max_linear_acc;
+  float max_angular_acc;
+};
+
+struct GlobalVelocity{};
+
+struct LocalVelocity{};
+
+struct GlobalAccel{};
+
+struct LocalAccel{};
+
+struct HeadingPivot{};
+
+struct PointPivot{};
+
+struct HeadingLine{};
+
+struct PointLine{};
+}  // namespace modes
+
+using ateam_game_state::Robot;
+
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::Off & params, const double t);
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::EstopBrake & params, const double t);
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::GlobalPosition & params, const double t);
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::GlobalVelocity & params, const double t);
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::LocalVelocity & params, const double t);
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::GlobalAccel & params, const double t);
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::LocalAccel & params, const double t);
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::HeadingPivot & params, const double t);
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::PointPivot & params, const double t);
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::HeadingLine & params, const double t);
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::PointLine & params, const double t);
+
+}  // ateam_controls_cpp::predict
+
+#endif  // ATEAM_CONTROLS_CPP__PREDICT_HPP_
---
+++ motion/ateam_controls/src/predict.cpp
@@ -0,0 +1,151 @@
+// Copyright 2026 A Team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
+#include "ateam_controls_cpp/predict.hpp"
+#include "ateam_controls/ateam_controls.h"
+#include "ateam_controls_cpp/exceptions.hpp"
+
+namespace ateam_controls_cpp::predict
+{
+
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::Off &, const double)
+{
+  return robot.pos;
+}
+
+ateam_geometry::Point PositionAtT(const Robot & robot, const modes::EstopBrake &, const double)
+{
+  return robot.pos;
+}
+
+ateam_geometry::Point PositionAtT(
+  const Robot & robot, const modes::GlobalPosition & params,
+  const double t)
+{
+  TrajectoryParams c_params{
+    .max_vel_linear = params.max_linear_vel,
+    .max_vel_angular = params.max_angular_vel,
+    .max_accel_linear = params.max_linear_acc,
+    .max_accel_angular = params.max_angular_acc
+  };
+  Vector6C c_start_state{
+    .data = {
+      static_cast<float>(robot.pos.x()),
+      static_cast<float>(robot.pos.y()),
+      static_cast<float>(robot.theta),
+      static_cast<float>(robot.vel.x()),
+      static_cast<float>(robot.vel.y()),
+      static_cast<float>(robot.omega)
+    }
+  };
+  Vector3C c_goal_state{
+    .x = params.global_x,
+    .y = params.global_y,
+    .z = params.global_theta
+  };
+  BangBangTraj3D trajectory;
+  if(const auto err = ateam_controls_traj_from_target_pose(c_start_state, c_goal_state, c_params,
+      &trajectory); err != ATEAM_CONTROLS_OK)
+  {
+    throw ControlsException(err);
+  }
+  Vector6C state;
+  if(const auto err = ateam_controls_traj_state_at(trajectory, t, &state);
+    err != ATEAM_CONTROLS_OK)
+  {
+    throw ControlsException(err);
+  }
+  return ateam_geometry::Point{state.data[0], state.data[1]};
+}
+
+ateam_geometry::Point PositionAtT(
+  const Robot & robot, const modes::GlobalVelocity & params,
+  const double t)
+{
+  (void)params;
+  (void)t;
+  return robot.pos;
+}
+
+ateam_geometry::Point PositionAtT(
+  const Robot & robot, const modes::LocalVelocity & params,
+  const double t)
+{
+  (void)params;
+  (void)t;
+  return robot.pos;
+}
+
+ateam_geometry::Point PositionAtT(
+  const Robot & robot, const modes::GlobalAccel & params,
+  const double t)
+{
+  (void)params;
+  (void)t;
+  return robot.pos;
+}
+
+ateam_geometry::Point PositionAtT(
+  const Robot & robot, const modes::LocalAccel & params,
+  const double t)
+{
+  (void)params;
+  (void)t;
+  return robot.pos;
+}
+
+ateam_geometry::Point PositionAtT(
+  const Robot & robot, const modes::HeadingPivot & params,
+  const double t)
+{
+  (void)params;
+  (void)t;
+  return robot.pos;
+}
+
+ateam_geometry::Point PositionAtT(
+  const Robot & robot, const modes::PointPivot & params,
+  const double t)
+{
+  (void)params;
+  (void)t;
+  return robot.pos;
+}
+
+ateam_geometry::Point PositionAtT(
+  const Robot & robot, const modes::HeadingLine & params,
+  const double t)
+{
+  (void)params;
+  (void)t;
+  return robot.pos;
+}
+
+ateam_geometry::Point PositionAtT(
+  const Robot & robot, const modes::PointLine & params,
+  const double t)
+{
+  (void)params;
+  (void)t;
+  return robot.pos;
+}
+
+}  // namespace ateam_controls_cpp::predict
